8dcf60ee30f11c7d64f1a4220290b1e5dae0221b
wisherp <wp4ty@virginia.edu>
2015-06-08 14:49:57 -0400

    File uploaded on behalf of the Open Science Framework

---
+++ GitHub testing/model.py
@@ -0,0 +1,24 @@
+from website.addons.base import AddonUserSettingsBase, AddonNodeSettingsBase
+from framework import GuidStoredObject
+from framework import fields
+import datetime
+import markdown
+
+class AddonRtmilkNodeSettings(AddonNodeSettingsBase):
+    def to_json(self, user):
+        return {}
+
+class NodeRtmilkPage(GuidStoredObject):
+
+    redirect_mode = 'redirect'
+
+    _id = fields.StringField(primary=True)
+
+    page_name = fields.StringField()
+    version = fields.IntegerField()
+    date = fields.DateTimeField(auto_now_add=datetime.datetime.utcnow)
+    is_current = fields.BooleanField()
+    content = fields.StringField()
+
+    user = fields.ForeignField('user')
+    node = fields.ForeignField('node')
